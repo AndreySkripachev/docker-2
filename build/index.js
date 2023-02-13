323ef95ebe9e493e170c7b50a8595a5d8df6476e
AndreySkripachev <andrey-skripachev@saritasa.com>
2023-02-13 15:38:19 +0700

    Remove build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const client = new pg_1.Client({
-    password: "root",
-    user: "root",
-    host: "postgres",
-});
-client.connect().then(() => console.log('Connected')).catch(console.log);
